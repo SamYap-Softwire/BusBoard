f8dca8e41c099eed06b031cd42404760154401c2
SamYap-Softwire <samyun.yap@softwire.com>
2023-07-03 16:02:12 +0100

    Part 2: from postcode to bus info

--- index.ts
+++ index.ts
@@ -1,53 +1,103 @@
 import fetch from "node-fetch";
-const prompt = require("prompt-sync")();
+import promptModule from "prompt-sync";
+const prompt = promptModule();
 
-const stopCode = prompt("Please enter a stop code: ");
-const url = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`;
+const main = async () => {
+  // const postCode = prompt("Please enter a post code: "); //  NW51TL
+  const postCode = "NW51TL";
+  const postCodeToLatLonAPI = `https://api.postcodes.io/postcodes/${postCode}`;
+  const latLon = await fetchByAwait(postCodeToLatLonAPI);
+  const [lat, lon] = getLatLon(latLon.result);
 
-const doByThen = () => {
-  fetch(url)
-    .then((response) => {
-      return response.json().then(output);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  let radius = 200;
+  const latLonToStopPointAPI = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=${radius}`;
+  // might want to check if at least 2 are returned, else increase radius (logarithmically)
+  const stopPoint = await fetchByAwait(latLonToStopPointAPI);
+  const idArray = getStopPoint(stopPoint.stopPoints);
+
+  const busArray = await generateBusArray(idArray);
+  const firstFiveBusArray = getBusInfo(busArray);
+  display(firstFiveBusArray);
 };
 
-async function doByAwait() {
-  try {
-    let data = await fetch(url);
-    let input = await data.json();
-    output(input);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// INTERFACES
 
-interface tflReturn {
+interface busObject {
   lineId: number;
   destinationName: string;
   timeToStation: number;
   timeToLive: Date;
 }
 
-const output = (data: tflReturn[]) => {
+interface latLonObject {
+  latitude: number;
+  longitude: number;
+}
+
+interface stopPointObject {
+  distance: number;
+  naptanId: string;
+}
+
+// FETCH FROM API
+
+const fetchByAwait = async (url: string): Promise<any> => {
+  try {
+    let data = await fetch(url);
+    let input = await data.json();
+    return input;
+  } catch (error) {
+    throw new Error(`${error}`); // help
+  }
+};
+
+// PROCESS API RESPONSE
+
+const getLatLon = (data: latLonObject) => {
+  return [data.latitude, data.longitude];
+};
+
+const getStopPoint = (data: stopPointObject[]) => {
   const sortedData = data.sort(
-    (a: tflReturn, b: tflReturn) => a.timeToStation - b.timeToStation
+    (a: stopPointObject, b: stopPointObject) => a.distance - b.distance
+  );
+  const firstTwo = sortedData.slice(0, 2);
+  const idArray: string[] = [];
+  for (const stop of firstTwo) {
+    idArray.push(stop.naptanId);
+  }
+  return idArray;
+};
+
+const generateBusArray = async (ids: string[]) => {
+  let allBusArray: busObject[] = [];
+  for (const id of ids) {
+    const stopPointToArrivalAPI = `https://api.tfl.gov.uk/StopPoint/${id}/Arrivals`;
+    const busArray = await fetchByAwait(stopPointToArrivalAPI);
+    allBusArray = allBusArray.concat(busArray);
+  }
+  return allBusArray;
+};
+
+const getBusInfo = (data: busObject[]) => {
+  const sortedData = data.sort(
+    (a: busObject, b: busObject) => a.timeToStation - b.timeToStation
   );
   const firstFive = sortedData.slice(0, 5);
-  for (const bus of firstFive) {
+  return firstFive;
+};
+
+const display = (arrivingBusArray: busObject[]) => {
+  for (const bus of arrivingBusArray) {
     const returnString = `ID: ${bus.lineId}, Destination: ${
       bus.destinationName
     }, Time of arrival: ${~~(bus.timeToStation / 60)} min`;
     console.log(returnString);
-    console.log(bus.timeToLive);
   }
+  // console.log(bus.timeToLive); // Check timeToLive of data (expire) - edge case of later bus catching up with earlier bus
 };
 
-doByAwait();
-// doByThen();
+// DISPLAY DATA
 
-// 490008660N
-
-// Check timeToLive of data (expire) - edge case of later bus catching up with earlier bus
+// ----- MAIN ----
+main();
